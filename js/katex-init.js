dd031d6e8090513dd1704edb992379ffb9cbbd67
ZYXian31 <2844352631@qq.com>
2026-03-01 16:58:39 +0800

    Site updated: 2026-03-01 16:58:37

---
+++ js/katex-init.js
@@ -0,0 +1,10 @@
+renderMathInElement(document.body, {
+  delimiters: [
+    { left: '$$',  right: '$$',  display: true  },
+    { left: '$',   right: '$',   display: false },
+    { left: '\\(', right: '\\)', display: false },
+    { left: '\\[', right: '\\]', display: true  }
+  ],
+  throwOnError: false,
+  strict: 'ignore'
+});
